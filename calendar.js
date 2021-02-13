56a9817c39b2b3fe3d997cbd3528c5ca86ce9f78
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2021-02-13 15:04:18 +0200

    validation

--- calendar.js
+++ calendar.js
@@ -101,8 +101,8 @@
         }
 
         function renderDayEvents(day, dayEventsContainer) {
-            const minHour = 10;
-            const maxHour = 18;
+            const minHour = day.minHour;
+            const maxHour = day.maxHour;
 
             for (let i = minHour; i <= maxHour; i++) {
                 let eventContainer = document.createElement('div');
@@ -124,8 +124,8 @@
                 eventContainer.style.position = 'absolute';
                 eventContainer.style.border = '1px #0080008f solid';
                 eventContainer.style.background = '#00800036';
-                eventContainer.style.top = (event.start - minHour) * hourDistance + 'px';
-                eventContainer.style.height = (event.end - event.start) * hourDistance + 'px';
+                eventContainer.style.top = (event.time.start - minHour) * hourDistance + 'px';
+                eventContainer.style.height = (event.time.end - event.time.start) * hourDistance + 'px';
                 eventContainer.innerHTML = `
                     <span>${event.name}</span>
                     <small data-role="delete-event" style="position: absolute; top: 0; right: 0">remove</small>
@@ -157,6 +157,12 @@
                     </div>
                     <div>
                         <div style="display: flex; width: 50%; justify-content:space-between">
+                            <div></div>
+                            <div><div id="timeReport"></div></div>
+                        </div>
+                    </div>
+                    <div>
+                        <div style="display: flex; width: 50%; justify-content:space-between">
                             <div><label for="eventStart">Start</label></div>
                             <div><input type="text" id="eventStart" name="eventStart"></div>
                         </div>
@@ -182,38 +188,119 @@
                 </form>
             `;
 
-            const usersSelect = document.getElementById('eventUsers');
-            for (const user of Object.values(calendarService.storage.users)) {
-                usersSelect.innerHTML += `
-                    <option value="${user.id}">${user.name}</option>
+            let form = document.getElementById('createEventForm');
+
+            {   // users select
+                const usersSelect = document.getElementById('eventUsers');
+                for (const user of Object.values(calendarService.storage.users)) {
+                    usersSelect.innerHTML += `
+                        <option value="${user.id}">${user.name}</option>
                 `;
+                }
             }
 
-            const daysSelect = document.getElementById('eventDays');
-            for (const day of Object.values(calendarService.storage.days)) {
-                daysSelect.innerHTML += `
-                    <option value="${day.id}">${day.name}</option>
+            {   // days select
+                const daysSelect = document.getElementById('eventDays');
+                for (const day of Object.values(calendarService.storage.days)) {
+                    daysSelect.innerHTML += `
+                        <option value="${day.id}">${day.name}</option>
                 `;
+                }
+                onDayChange(daysSelect);
+                daysSelect.addEventListener('change', () => onDayChange(daysSelect));
+
+                function onDayChange(select) {
+                    removeValidation(form);
+                    let dayId = Number(select.value);
+                    let freeTimesElement = document.getElementById('timeReport');
+
+                    let freeTimes = calendarService.getAvailableTime(dayId);
+                    if (freeTimes.length) {
+                        freeTimesElement.innerHTML = freeTimes.reduce((html, time) => {
+                            html += `<i style="display: inline-block; margin: 0 3px">${time.start}-${time.end}</i>`;
+                            return html;
+                        }, 'Free time: ')
+                    } else {
+                        freeTimesElement.innerHTML = 'No available time left';
+                    }
+                }
             }
 
             document.getElementById('createEventForm').addEventListener('submit', (event) => {
                 event.preventDefault();
-                let elements = event.target.elements;
+                let form = event.target;
+                let elements = form.elements;
 
-                calendarService.createEvent(
-                    Number(elements.eventDays.value),
-                    elements.eventName.value,
-                    Number(elements.eventStart.value),
-                    Number(elements.eventEnd.value),
-                    [...elements.eventUsers.querySelectorAll('option:checked')].map(elem => Number(elem.value))
-                )
+                removeValidation(form);
+
+                let validationContext = {valid: true};
+                let eventNameElement = elements.eventName;
+                let dayElement = elements.eventDays;
+                let eventStartElement = elements.eventStart;
+                let eventEndElement = elements.eventEnd;
+                let eventUsersElement = elements.eventUsers;
+                let timeReportElement = form.querySelector('#timeReport');
+
+                let name = eventNameElement.value
+                if (eventNameElement.value === '') {
+                    appendValidation(eventNameElement, 'Enter event name', validationContext);
+                }
+                let dayId;
+                if (dayElement.value === '') {
+                    appendValidation(dayElement, 'Select day', validationContext);
+                } else {
+                    dayId = Number(dayElement.value);
+                }
+                let start;
+                if (eventStartElement.value === '') {
+                    appendValidation(eventStartElement, 'Enter start time', validationContext);
+                } else if (isNaN(eventStartElement.value) || Number(eventStartElement.value) < 0) {
+                    appendValidation(eventStartElement, 'Start time should have a positive numeric value', validationContext);
+                } else {
+                    start = Number(eventStartElement.value);
+                }
+                let end;
+                if (eventEndElement.value === '') {
+                    appendValidation(eventEndElement, 'Enter end time');
+                } else if (isNaN(eventEndElement.value) || Number(eventEndElement.value) < 0) {
+                    appendValidation(eventEndElement, 'End time should have a positive numeric value', validationContext);
+                } else {
+                    end = Number(eventEndElement.value);
+                }
+                let users = [...eventUsersElement.querySelectorAll('option:checked')].map(elem => Number(elem.value));
+                if (users.length === 0) {
+                    appendValidation(eventUsersElement, 'Select at least one user', validationContext);
+                }
+                if (validationContext.valid && !calendarService.isEventTimeValid(dayId, start, end)) {
+                    appendValidation(timeReportElement, `${start}-${end} time is occupied`, validationContext);
+                }
+
+                if (validationContext.valid) {
+                    calendarService.createEvent(
+                        dayId,
+                        name,
+                        start,
+                        end,
+                        users
+                    )
+                    renderCalendarScreen();
+                }
+
+            });
 
 
-                renderCalendarScreen();
-            })
-        }
+            function appendValidation(element, message, context) {
+                let validationElement = document.createElement('div');
+                validationElement.setAttribute('validation-message', '');
+                validationElement.innerHTML = `<div style="color: red">${message}</div>`;
+                element.after(validationElement);
+                context.valid = false;
+            }
 
-        function renderCreateEvent() {
+            function removeValidation(form) {
+                form.querySelectorAll('[validation-message]').forEach(e => e.remove());
+            }
+
 
         }
 
--- calendar.service.js
+++ calendar.service.js
@@ -37,6 +37,60 @@ class CalendarService {
         });
     }
 
+    isEventTimeValid(dayId, start, end) {
+        let day = this.storage.days[dayId];
+        if (start < day.minHour) {
+            return false;
+        }
+        if (end > day.maxHour) {
+            return false;
+        }
+        if (start >= end) {
+            return false;
+        }
+
+        let events = day.events.sort((e1, e2) => e2.time.start - e1.time.start);
+        for (let event of events) {
+            // inside
+            if (start >= event.time.start && start < event.time.end ||
+                end > event.time.start && end < event.time.end) {
+                return false;
+            }
+            //overlaps
+            if (start <= event.time.start && end >= event.time.end) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    getAvailableTime(dayId) {
+        let result = [];
+        let day = this.storage.days[dayId];
+
+
+        if (day.events.length) {
+            let startFree = day.minHour;
+
+            day.events
+                .sort((e1, e2) => e2.time.start - e1.time.start)
+                .forEach(event => {
+                    if (startFree < event.time.start) {
+                        result.push(new TimeInterval(startFree, event.time.start));
+                    }
+                    startFree = event.time.end;
+                });
+            if (startFree < day.maxHour) {
+                result.push(new TimeInterval(startFree, day.maxHour));
+            }
+
+        } else {
+            result.push(new TimeInterval(day.minHour, day.maxHour));
+        }
+        return result;
+    }
+
 
 }
 
@@ -66,6 +120,8 @@ class Day extends Entity {
         super(id);
         this.name = name;
         this.events = [];
+        this.minHour = 10;
+        this.maxHour = 18;
     }
 }
 
@@ -74,11 +130,19 @@ class Event extends Entity {
     constructor(id, name, start, end) {
         super(id);
         this.name = name;
-        this.start = start;
-        this.end = end;
+        this.time = new TimeInterval(start, end);
         this.users = [];
     }
 }
+
+class TimeInterval {
+
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
 
 class User extends Entity {
 
